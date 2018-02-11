68e5ff1b5e1a02aaec214472bbdf4fa7ec841396
Ariana <ariana@Arianas-Air.fios-router.home>
2018-02-11 15:14:11 -0500

    add party flavicon & confetti animations

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require('path');
 app.use("/views", express.static(__dirname + '/views'));
 app.use("/scripts",express.static(__dirname + '/scripts'));
 app.use("/styles", express.static(__dirname + '/styles'));
+app.use("/assets", express.static(__dirname + '/assets'));
 
 app.get('/', function(req,res) {
   res.sendFile("index.html", {"root": __dirname});
@@ -15,7 +16,7 @@ app.get('/the-day',function(req,res){
 });
 
 app.get('/travel',function(req,res){
-  res.sendFile(path.resolve('./views/sights.html'));
+  res.sendFile(path.resolve('./views/travel.html'));
 });
 
 app.get('/registry',function(req,res){
@@ -23,11 +24,15 @@ app.get('/registry',function(req,res){
 });
 
 app.get('/rsvp',function(req,res){
-  res.sendFile(path.resolve('./views/registry.html'));
+  res.sendFile(path.resolve('./views/rsvp.html'));
 });
 
-app.get('/welcome',function(req,res){
-  res.sendFile(path.resolve('./views/registry.html'));
+app.get('/party-time',function(req,res){
+  res.sendFile(path.resolve('./views/party-time.html'));
+});
+
+app.get('/assets',function(req,res){
+  res.sendFile(path.resolve('./assets'));
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
