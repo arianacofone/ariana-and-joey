b8b7a1229e0c071e829cc9cd6dc9814355e54c31
Ariana Dziedzic <ariana.dziedzic@gmail.com>
2018-02-20 07:14:30 -0500

    add email response when rsvped

--- index.js
+++ index.js
@@ -2,14 +2,47 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
+var nodemailer = require('nodemailer');
+require('dotenv').config()
+
 app.use("/views", express.static(__dirname + '/views'));
 app.use("/scripts",express.static(__dirname + '/scripts'));
 app.use("/styles", express.static(__dirname + '/styles'));
-app.use("/assets", express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function(req,res) {
   res.sendFile("index.html", {"root": __dirname});
 })
+
+app.post("/test", function (req, res) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    port: 25,
+    auth: {
+      user: process.env.NODE_EMAIL,
+      pass: process.env.NODE_PASSWORD
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  let HelperOptions = {
+    from: 'arianaandjoey@gmail.com',
+    to: 'arianaandjoey@gmail.com',
+    subject: 'You RSVPed to our Wedding!',
+    html: '<h1>Wow, you are awesome!</h1><p>The most awesome</p><p>Yes you are!</p>'
+  };
+
+  transporter.sendMail(HelperOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("The message was sent!");
+    console.log(info);
+  });
+});
 
 app.get('/the-day',function(req,res){
   res.sendFile(path.resolve('./views/the-day.html'));
